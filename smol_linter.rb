8d2cfbc77ff544fbe5cd68afe73ee1e5906ca09b
iamricks <richardsantos@Richards-MacBook-Pro.local>
2022-01-19 12:22:02 -0500

    fix spacing cuz sublime lmao

--- smol_linter.rb
+++ smol_linter.rb
@@ -9,6 +9,6 @@ modified_file_paths = %x(git diff --name-only).split("\n")
 
 # Loop through files, check the syntax and print the status
 modified_file_paths.each do |path|
-	next if !path.include?('.rb')
-	print(%x(ruby -c #{path}))
+  next if !path.include?('.rb')
+  print(%x(ruby -c #{path}))
 end
